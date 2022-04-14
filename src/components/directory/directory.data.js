ad15304ac9539f122c3fe2643862fcbf14497f3a
JanSeulin <jseulin@DESKTOP-JOVTUOR.localdomain>
2022-04-14 20:31:59 -0300

    compressed the homepage images for better performance

--- src/components/directory/directory.data.js
+++ src/components/directory/directory.data.js
@@ -1,37 +1,37 @@
 export const HOMEPAGE_DATA = [
   {
     title: 'Motherboards',
-    imageUrl: 'https://i.ibb.co/bP4sV39/motherboard.jpg',
+    imageUrl: 'https://i.ibb.co/PT9VNT2/motherboard.webp',
     id: 1,
     linkUrl: 'shop/motherboards',
   },
   {
     title: 'CPUs',
-    imageUrl: 'https://i.ibb.co/FYzQRKM/CPU.jpg',
+    imageUrl: 'https://i.ibb.co/C9dMfdw/CPU.webp',
     id: 2,
     linkUrl: 'shop/cpus',
   },
   {
     title: 'Graphic Cards',
-    imageUrl: 'https://i.ibb.co/nzqQ0PK/GPU.jpg',
+    imageUrl: 'https://i.ibb.co/885gGJ5/GPU.webp',
     id: 3,
     linkUrl: 'shop/gpus',
   },
   {
     title: 'Memory',
-    imageUrl: 'https://i.ibb.co/JcB92qV/memory.jpg',
+    imageUrl: 'https://i.ibb.co/Y7SxDS9/memory.webp',
     id: 4,
     linkUrl: 'shop/memories',
   },
   {
     title: 'Storage',
-    imageUrl: 'https://i.ibb.co/VJS1Cnw/storage.jpg',
+    imageUrl: 'https://i.ibb.co/5Lc44rN/storage.webp',
     id: 5,
     linkUrl: 'shop/motherboards',
   },
   {
     title: 'Peripherals',
-    imageUrl: 'https://i.ibb.co/3RdgPkd/mouse.jpg',
+    imageUrl: 'https://i.ibb.co/qjjwFjR/mouse.webp',
     id: 6,
     linkUrl: 'shop/peripherals',
   },
